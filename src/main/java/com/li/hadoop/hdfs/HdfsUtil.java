9a37ee37efccfbe2988cdc1e4ed8583518c60148
fjl121029xx <sishengqikuo_xx@163.com>
2018-03-08 17:23:29 +0800

    hdfs工作原理

--- src/main/java/com/li/hadoop/hdfs/HdfsUtil.java
+++ src/main/java/com/li/hadoop/hdfs/HdfsUtil.java
@@ -14,7 +14,7 @@ public class HdfsUtil {
     public static void main(String[] args) throws IOException {
 
         Configuration entries = new Configuration();
-    //文件系统
+        //文件系统
         FileSystem fs = FileSystem.get(entries);
 
         Path path = new Path("hdfs://hadoop:9000/jdk-7u65-linux-i586.tar.gz");
@@ -25,7 +25,6 @@ public class HdfsUtil {
 
         IOUtils.copy(bis, bos);
     }
-
 
 
 }
