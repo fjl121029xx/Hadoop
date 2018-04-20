54bb559edc84b979f1ec7ae94970339d2d1cb3d5
fjl121029xx <sishengqikuo_xx@163.com>
2018-04-20 17:18:40 +0800

    kafka 配置参数

--- src/main/java/com/li/kafka/ProducerDemo.java
+++ src/main/java/com/li/kafka/ProducerDemo.java
@@ -6,22 +6,25 @@ import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 
+/**
+ * https://www.cnblogs.com/jun1019/p/6656223.html
+ */
 public class ProducerDemo {
-	public static void main(String[] args) throws Exception {
-		Properties props = new Properties();
-		props.put("zk.connect", "192.168.233.137:2181,192.168.233.138:2181,192.168.233.139:2181");
-		props.put("metadata.broker.list","192.168.233.137:9092,192.168.233.138:9092,192.168.233.139:9092");
-		props.put("serializer.class", "kafka.serializer.StringEncoder");
-		ProducerConfig config = new ProducerConfig(props);
-		Producer<String, String> producer = new Producer<String, String>(config);
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put("zk.connect", "192.168.233.137:2181,192.168.233.138:2181,192.168.233.139:2181");
+        props.put("metadata.broker.list", "192.168.233.137:9092,192.168.233.138:9092,192.168.233.139:9092");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        ProducerConfig config = new ProducerConfig(props);
+        Producer<String, String> producer = new Producer<String, String>(config);
 
-		// ����ҵ����Ϣ
-		// ��ȡ�ļ� ��ȡ�ڴ����ݿ� ��socket�˿�
-		for (int i = 1; i <= 100; i++) {
-			Thread.sleep(500);
-			producer.send(new KeyedMessage<String, String>("wordcount",
-					"i said i love you baby for" + i + "times,will you have a nice day with me tomorrow"));
-		}
+        // ����ҵ����Ϣ
+        // ��ȡ�ļ� ��ȡ�ڴ����ݿ� ��socket�˿�
+        for (int i = 1; i <= 100; i++) {
+            Thread.sleep(500);
+            producer.send(new KeyedMessage<String, String>("wordcount",
+                    "i said i love you baby for" + i + "times,will you have a nice day with me tomorrow"));
+        }
 
-	}
+    }
 }
