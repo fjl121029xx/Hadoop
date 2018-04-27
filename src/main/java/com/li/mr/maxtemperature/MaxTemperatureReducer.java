70640a026584ffe7b9b560727838273b5912fdd9
fjl121029xx <sishengqikuo_xx@163.com>
2018-04-27 16:31:56 +0800

    最高温度

---
+++ src/main/java/com/li/mr/maxtemperature/MaxTemperatureReducer.java
@@ -0,0 +1,31 @@
+package com.li.mr.maxtemperature;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        super.setup(context);
+    }
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+        int maxValue = Integer.MIN_VALUE;
+        for (IntWritable value : values) {
+            maxValue = Math.max(maxValue, value.get());
+        }
+
+        context.write(key, new IntWritable(maxValue));
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        super.cleanup(context);
+    }
+}
