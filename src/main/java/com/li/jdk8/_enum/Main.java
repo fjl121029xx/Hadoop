75ba531af92eee6a9cca7484cc4f1834f7e4d54b
e <sishengqikuo_xx@163.com>
2018-11-16 09:59:06 +0800

    2018-11-16  ...1

---
+++ src/main/java/com/li/jdk8/_enum/Main.java
@@ -0,0 +1,15 @@
+package com.li.jdk8._enum;
+
+import org.jruby.ext.ffi.Enum;
+
+public class Main {
+    /**
+     * https://www.cnblogs.com/happyPawpaw/archive/2013/04/09/3009553.html
+     */
+    public static void main(String[] args) {
+
+        EnumTest mon = EnumTest.MON;
+
+        System.out.println(mon);
+    }
+}
