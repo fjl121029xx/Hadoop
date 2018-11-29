c301d1800778d9834569a0ca54f50d521b49dede
e <sishengqikuo_xx@163.com>
2018-11-29 18:36:54 +0800

    2018-11-29  ..1

---
+++ src/main/java/com/li/mahout/RecommenderIntro.java
@@ -0,0 +1,34 @@
+package com.li.mahout;
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+
+import java.io.File;
+import java.util.List;
+
+public class RecommenderIntro {
+
+    public static void main(String[] args) throws Exception {
+
+
+        DataModel model = new FileDataModel(new File("data/ml100K/ratings.data"));
+
+        PearsonCorrelationSimilarity similarity = new PearsonCorrelationSimilarity(model);
+
+        NearestNUserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
+
+        Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+
+        List<RecommendedItem> recommendations = recommender.recommend(1, 10);
+
+        for (RecommendedItem recommendation : recommendations) {
+
+            System.out.println(recommendation);
+        }
+    }
+}
---
+++ src/main/java/com/li/mahout/runRecommenderEvaluator.java
@@ -0,0 +1,48 @@
+package com.li.mahout;
+
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.RMSRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.apache.mahout.common.RandomUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 配置并评估一个推荐程序
+ */
+public class runRecommenderEvaluator {
+
+    public static void main(String[] args) throws Exception {
+
+//        RandomUtils.useTestSeed();
+        DataModel model = new FileDataModel(new File("data/ml100K/ratings.data"));
+
+        RecommenderEvaluator evaluator = new RMSRecommenderEvaluator();
+
+        RecommenderBuilder builder = new RecommenderBuilder() {
+
+            @Override
+            public Recommender buildRecommender(DataModel dataModel) throws TasteException {
+                UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+                NearestNUserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
+
+                return new GenericUserBasedRecommender(model, neighborhood, similarity);
+            }
+        };
+
+        double score = evaluator.evaluate(builder, null, model, 0.7, 1);
+        System.out.println(score);
+
+    }
+}
