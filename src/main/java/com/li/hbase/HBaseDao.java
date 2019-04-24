e1992ba30e6ed8b75e29776c6fae17bed3c3cba2
fjl121029xx <sishengqikuo_xx@163.com>
2019-04-24 14:39:43 +0800

    2019-04-24   ...1

--- src/main/java/com/li/hbase/HBaseDao.java
+++ src/main/java/com/li/hbase/HBaseDao.java
@@ -15,24 +15,20 @@ public class HBaseDao {
 
 
         Configuration conf = HBaseConfiguration.create();
-//        conf.set("hbase.zookeeper.quorum","192.168.233.137:2181,192.168.233.138:2181,192.168.233.139:2181");
+        conf.set("hbase.zookeeper.quorum","192.168.100.68:2181,192.168.100.70:2181,192.168.100.72:2181");
 
 
         HBaseAdmin hBaseAdmin = new HBaseAdmin(conf);
 
-        TableName name = TableName.valueOf("feifei");
+        TableName name = TableName.valueOf("grasp");
         HTableDescriptor descriptor = new HTableDescriptor(name);
 
         HColumnDescriptor base_info = new HColumnDescriptor("base_info");
         base_info.setMaxVersions(3);
         descriptor.addFamily(base_info);
 
-        HColumnDescriptor extra_info = new HColumnDescriptor("extra_info");
-        base_info.setMaxVersions(3);
-        descriptor.addFamily(extra_info);
 
         hBaseAdmin.createTable(descriptor);
-        System.out.println("1111");
 
     }
 }
